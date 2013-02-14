5432d5e9ec1ed10e2c0c334c4a069e5c5be94f65
khaledsaied <kaledsaied@gmail.com>
2013-02-14 13:37:42 +0100

    SWT Exercise 3.2 tested on MonoDevelop for Mac

---
+++ Exercise 3/SWT_Ex3.2[MonoDevelop]/WeighingSystem/WeighingSystem.Test/WeighingUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+
+namespace WeighingSystem.Test
+{
+    [TestFixture]
+    public class WeighingUnitTest
+    {
+        [Test]
+        public void GetWeight_Test()
+        {
+            Log log = new Log("khaled", false);
+            Weight weight = new Weight();
+            IWeighingUnit Interface = new WeighingUnit(log, weight);
+            Assert.AreEqual(Interface.GetWeight(), 100);
+        }
+     
+    }
+}
---
+++ Exercise 3/SWT_Ex3.2[MonoDevelop]/WeighingSystem/WeighingSystem/Weight.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeighingSystem
+{
+
+    public interface IWeight
+    {
+        uint Read();
+        double GetResolution();
+    }
+    
+    public class Weight : IWeight
+    {
+        public Weight()
+        {
+        }
+
+        public uint Read()              // (*) IWeight
+        {
+            return 0;
+        }
+
+        public double GetResolution()   // (*) IWeight
+        {
+            return 0.0;
+        }
+    }
+}
